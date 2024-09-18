cd2e27a77ef3036d56004b8b34c22f6118e9a5d3
Heiko Bensch <heiko@bensch.family>
2024-09-18 19:57:04 +0200

    1: The label text has been moved to the message properties for the student forms.

--- src/main/java/de/bensch/course/controller/StudentController.java
+++ src/main/java/de/bensch/course/controller/StudentController.java
@@ -6,52 +6,66 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import static de.bensch.course.controller.UrlMappings.*;
+
 @Controller
 public class StudentController {
 
     private final StudentService studentService;
 
+    @ModelAttribute("urlMappings")
+    public UrlMappings urlMappings() {
+        //noinspection InstantiationOfUtilityClass
+        return new UrlMappings();
+    }
+
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
-    @GetMapping("/students")
+    @GetMapping(STUDENT_LIST)
     public String courses(Model model) {
-        model.addAttribute("studentsList", studentService.findAll());
-        return "students";
+        model.addAttribute("studentList", studentService.findAll());
+        return STUDENT_LIST;
     }
 
-    @GetMapping("/studentCreate")
-    public String createCourseForm(Model model) {
+    @GetMapping(STUDENT_CREATE)
+    public String createCourse(Model model) {
         model.addAttribute("student", new Student(null, null, 0, null));
-        return "studentForm";
+        return STUDENT_CREATE;
     }
 
-    @PostMapping("/studentCreate")
+    @PostMapping(STUDENT_CREATE)
     public String createStudentSubmit(@ModelAttribute Student student) {
+        String result;
+        if (student.getId() == null) {
+            result = "redirect:" + STUDENT_CREATE;
+        } else {
+            result = "redirect:" + STUDENT_LIST;
+        }
         studentService.save(student);
-        return "redirect:/students";
+        return result;
     }
 
-    @GetMapping("/studentEdit/{id}")
+    @GetMapping(STUDENT_EDIT)
     public String editStudentForm(@PathVariable("id") Long id, Model model) {
         Student student = studentService.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid student ID:" + id));
         model.addAttribute("student", student);
-        return "studentForm";
+        return STUDENT_CREATE;
     }
 
-    @PostMapping("/studentEdit/{id}")
+    @PostMapping(STUDENT_EDIT)
     public String editStudentSubmit(@PathVariable("id") Long id, @ModelAttribute Student student) {
         studentService.save(student);
-        return "redirect:/students";
+        return "redirect:"+STUDENT_LIST;
     }
 
-    @GetMapping("/studentDelete/{id}")
+    @GetMapping(STUDENT_DELETE)
     public String deleteStudent(@PathVariable("id") Long id) {
         Student student = studentService.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid student ID:" + id));
         studentService.delete(student);
-        return "redirect:/students";
+        return "redirect:"+STUDENT_LIST;
     }
 }
